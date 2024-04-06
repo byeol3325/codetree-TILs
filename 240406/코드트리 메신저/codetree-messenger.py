b00cb5778d8dace159eaf148366b573b220b69b3
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-04-06 23:29:59 +0900

    Title: 코드트리 메신저, Time: 8061ms, Memory: 45MB, Status: Time Limit Exceed - CodeTree

--- 240406/코드트리 메신저/codetree-messenger.py
+++ 240406/코드트리 메신저/codetree-messenger.py
@@ -26,30 +26,32 @@ def change_power(chat_rooms, idx, power):
     chat_rooms[idx].authority = power
 
 def swap_parents(chat_rooms, idx1, idx2):
-    # 4, 5
-    parent1 = chat_rooms[idx1].parent # 1
-    parent2 = chat_rooms[idx2].parent # 2
+    parent1 = chat_rooms[idx1].parent
+    parent2 = chat_rooms[idx2].parent
     if parent1 == parent2:
         return
 
-    chat_rooms[parent1].nodes = [node for node in chat_rooms[parent1].nodes if node != idx1] + [idx2]
-    chat_rooms[parent2].nodes = [node for node in chat_rooms[parent2].nodes if node != idx2] + [idx1]
+    chat_rooms[parent1].nodes.remove(idx1)
+    chat_rooms[parent1].nodes.append(idx2)
+    chat_rooms[parent2].nodes.remove(idx2)
+    chat_rooms[parent2].nodes.append(idx1)
 
-    chat_rooms[idx1].parent, chat_rooms[idx2].parent = chat_rooms[idx2].parent, chat_rooms[idx1].parent
+    chat_rooms[idx1].parent, chat_rooms[idx2].parent = parent2, parent1
+
 
 def count_notifiable(chat_rooms, idx, depth):
-    global N, count
-    if depth <= chat_rooms[idx].authority:
-        if depth != 0:
-            #print("Node : ", idx)
-            count+=1
-    
-    nodes = chat_rooms[idx].nodes
-    
-    for node in nodes:
-        if chat_rooms[node].alarm == True:
-            count_notifiable(chat_rooms, node, depth+1)
-    return 
+    count = 0
+    stack = [(idx, 0)]  # (node index, depth)
+
+    while stack:
+        node_idx, depth = stack.pop()
+        if depth <= chat_rooms[node_idx].authority and depth != 0:
+            count += 1
+
+        for child_idx in chat_rooms[node_idx].nodes:
+            if chat_rooms[child_idx].alarm:
+                stack.append((child_idx, depth + 1))
+    print(count)
 
 N, Q = map(int, input().split())
 parents_authorities = list(map(int, input().split())) # 100 p1 p2 ... pN a1 a2 ... aN
@@ -64,9 +66,9 @@ for _ in range(1, Q):
     elif q[0] == 400: # 400 c1 c2
         swap_parents(chat_rooms, q[1], q[2])
     else: # 500 c
-        count = 0
+        #count = 0
         count_notifiable(chat_rooms, q[1], 0)
-        print(count)
+        #print(count)
     
     #for i in range(N+1):
     #    print(i, chat_rooms[i].nodes)
