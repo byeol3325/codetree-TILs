915fc076fc77abdfb14c8e5d6bd18da4f0bcbec6
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-04-07 15:04:08 +0900

    Title: 코드트리 빵, Time: 1764ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240407/코드트리 빵/codetree-mon-bread.py
+++ 240407/코드트리 빵/codetree-mon-bread.py
@@ -3,33 +3,28 @@ from collections import deque
 
 input = sys.stdin.readline
 dx = [-1, 0, 0, 1]
-dy = [0, -1, 1, 0] # 우선순위 고려 (상좌우하)
+dy = [0, -1, 1, 0]
 
-######################## 초기 값 설정 ########################
-N, M = map(int, input().split()) 
-matrix = [0] * N # matrix 값 저장
-base_camps = [] # base_camp 위치들 저장
-for i in range(N):
-    line = list(map(int, input().split()))
-    for j in range(N):
-        if line[j] == 1:
-            base_camps.append([i, j])
-    matrix[i] = line
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+base = []
+for i in range(n):
+    for j in range(n):
+        if a[i][j] != 0:
+            base.append([i, j])
 
-stores = [0] * M # store 위치 저장
-for i in range(M):
+cu = [0] * m
+for i in range(m):
     x, y = map(int, input().split())
-    stores[i] = [x-1, y-1] # 위치 보정. 0,0에서 시작해서 -1, -1 해줘야함
-#############################################################
-
+    cu[i] = [x-1, y-1]
 
 base_start = []
-ex = [[0] * N for _ in range(N)] # 도착하면 이제 안 움직임
-for x, y in stores:
+ex = [[0] * n for _ in range(n)]
+for x, y in cu:
     q = deque()
     q.append([x, y])
     ex[x][y] = 1
-    check_mov = [[0] * N for _ in range(N)]
+    check_mov = [[0] * n for _ in range(n)]
     check_mov[x][y] = 1
     flag = 0
     while q:
@@ -37,9 +32,9 @@ for x, y in stores:
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
+            if 0 <= nx < n and 0 <= ny < n:
                 if check_mov[nx][ny] == 0 and ex[nx][ny] == 0:
-                    if matrix[nx][ny] == 1:
+                    if a[nx][ny] == 1:
                         ex[nx][ny] = 1
                         base_start.append([nx, ny])
                         flag = 1
@@ -51,9 +46,9 @@ for x, y in stores:
         if flag:
             break
 
-ex = [[0] * N for _ in range(N)]
-check_mov = [[[0] * M for _ in range(N)] for _ in range(N)]
-check_end = [0] * M
+ex = [[0] * n for _ in range(n)]
+check_mov = [[[0] * m for _ in range(n)] for _ in range(n)]
+check_end = [0] * m
 cnt, idx = 1, 1
 q = deque()
 bx, by = base_start[0][0], base_start[0][1]
@@ -61,7 +56,7 @@ q.append([bx, by, 0])
 ex[bx][by] = 1
 check_mov[bx][by][0] = 1
 while q:
-    if 0 < idx < M:
+    if 0 < idx < m:
         bx, by = base_start[idx][0], base_start[idx][1]
         ex[bx][by] = 1
         check_mov[bx][by][idx] = 1
@@ -74,9 +69,9 @@ while q:
         for k in range(4):
             nx = x + dx[k]
             ny = y + dy[k]
-            if 0 <= nx < N and 0 <= ny < N:
+            if 0 <= nx < n and 0 <= ny < n:
                 if check_mov[nx][ny][i] == 0 and ex[nx][ny] == 0:
-                    if nx == stores[i][0] and ny == stores[i][1]:
+                    if nx == cu[i][0] and ny == cu[i][1]:
                         ex[nx][ny] = 1
                         check_end[i] = 1
                     else:
@@ -84,7 +79,7 @@ while q:
 
     if 0 not in check_end:
         break
-    if 0 < idx < M:
+    if 0 < idx < m:
         q.append([bx, by, idx])
     idx += 1
 
