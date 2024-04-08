50a890f5368904cd7cb45e7acdd1e1cf52492ddd
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-04-08 10:52:51 +0900

    Title: 산타의 선물 공장 2, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240408/산타의 선물 공장 2/santa-gift-factory-2.py
+++ 240408/산타의 선물 공장 2/santa-gift-factory-2.py
@@ -74,10 +74,18 @@ def Rule2(q):
     if num == 0: # 없으면 끝
         print(nums[m_dst])
         return
-
-    for i in range(num):
-        Move(m_src, m_dst)
+    nums[m_dst] += num; nums[m_src] = 0
     
+    h = head[m_src]; head[m_src] = -1
+    t = tail[m_src]; tail[m_src] = -1
+    h_dst = head[m_dst]; before[h_dst] = t
+    before[h] = -1
+    after[t] = tail[m_dst]
+
+    head[m_dst] = h
+    if tail[m_dst] == -1:
+        tail[m_dst] = t
+
     print(nums[m_dst])
     return
 def Move(m_src, m_dst):
@@ -146,7 +154,6 @@ def Rule5(q): # 500 p_num
     b = after[p_num]
     print(a + 2*b)
     return
-
 def Rule6(q): # 600 b_num
     b_num = q[1]
     a = head[b_num]
@@ -156,156 +163,30 @@ def Rule6(q): # 600 b_num
     print(a + 2*b + 3*c)
     return
 
-"""
-def Rule2(q):
-    global belts, max_belts
-    m_src = q[1]; m_dst = q[2]
-    n = len(belts[m_src])
-    for _ in range(n):
-        hq.heappush(belts[m_dst], hq.heappop(belts[m_src]))
-        hq.heappush(max_belts[m_dst], hq.heappop(max_belts[m_src]))
-    print(len(belts[m_dst]))
-    return
-
-# 3. 앞 물건만 교체 (m_src) <=> (m_dst)  ///  print(m_dst)
-#    mm_src 벨트에 있는 선물 중 가장 앞에 있는 선물을 m_dst 벨트 선물들 중 가장 앞에 있는 선물과 교체
-#   없으면 있는 곳에서만 옮기기 
-#    옮긴 뒤에 m_dst벨트에 있는 선물 갯수 출력
-def Rule3(q):
-    global belts, max_belts
-    m_src = q[1]; m_dst = q[2]
-    n1 = len(belts[m_src]); n2 = len(belts[m_dst])
-    src = []; max_src = [];
-    dst = []; max_dst = [];
-    if n1 != 0:
-        n1_front = hq.heappop(belts[m_src])
-        hq.heappush(dst, n1_front)
-        hq.heappush(max_dst, -n1_front)
-    
-    if n2 != 0:
-        n2_front = hq.heappop(belts[m_dst])
-        hq.heappush(src, n2_front)
-        hq.heappush(max_src, n2_front)
-    
-    for i in range(n1-1):
-        n1_front = hq.heappop(belts[m_src])
-        hq.heappush(src, n1_front)
-        hq.heappush(max_src, -n1_front)
-    
-    for i in range(n2-1):
-        n2_front = hq.heappop(belts[m_dst])
-        hq.heappush(dst, n2_front)
-        hq.heappush(max_dst, -n2_front)
-    
-    belts[m_src] = src; max_belts[m_src] = max_src
-    belts[m_dst] = dst; max_belts[m_dst] = max_dst
-
-    print(len(belts[m_dst]))
-    return
-
-# 4. 물건 나누기 (m_src) => (m_dst)    print(m_dst)
-#    m_src 번째 벨트에 있는 선물 갯수 n이라고 할 때 가장 앞에앞에서 floor(n/2)까지 있는 선물을 m_dst 벨트 앞으로 옮김
-#    1개라면 안 옮김. 옮긴 뒤 m_dst에 선물 갯수 출력 
-def Rule4(q): # 400 m_src m_dst
-    global belts, max_belts
-    m_src = q[1]; m_dst = q[2]
-    n1 = len(belts[m_src]); n2 = len(belts[m_dst])
-    
-    if n1 < 2:
-        return
-    
-    mov_num = n1//2
-    #if n1%2 == 1:
-    #    mov_num += 1
-    
-    for _ in range(mov_num):
-        n1_front = hq.heappop(belts[m_src])
-        hq.heappush(belts[m_dst], n1_front)
-        hq.heappush(max_belts[m_dst], -n1_front)
-    
-    max_src = []
-    for v in belts[m_src]:
-        hq.heappush(max_src, -v)
-    
-    max_belts[m_src] = max_src
-    print(len(belts[m_dst]))
-    return
-
-def Rule5(q): # 500 p_num
-    global belts, max_belts
-    idx = 0
-    for i, b in enumerate(belts):
-        if q[1] in b:
-            idx = i; break
-    
-    a = -1; b = -1;
-    src = copy.deepcopy(belts[idx]); max_src = copy.deepcopy(max_belts[idx])
-
-    while belts[idx]:
-        now = hq.heappop(belts[idx])
-        if now == q[1]:
-            break
-        a = now
-    
-    while max_belts[idx]:
-        now = hq.heappop(max_belts[idx])
-        if -now == q[1]:
-            break
-        b = -now
-    
-    belts[idx] = src; max_belts[idx] = max_src
-    print(a + 2*b)
-    return
-
-def Rule6(q): # 600 b_num
-    if len(belts[q[1]]) == 0:
-        print(-3)
-        return
-    src = copy.deepcopy(belts[q[1]]); max_src = copy.deepcopy(max_belts[q[1]])
-    a = hq.heappop(belts[q[1]]); b = -hq.heappop(max_belts[q[1]])
-    belts[q[1]] = src; max_belts[q[1]] = max_src
-    print(a + 2*b + 3*len(belts[q[1]]))
-    return
-
-belts = []; max_belts = [];
-"""
 stop = -1
 for i in range(1, Q):
     q = list(map(int, input().split()))
     if i == stop:
         print("TIME : ", i)
-    if q[0] == 100: # 100 n m B_NUM1 B_NUM2 ... B_NUMm 항상 처음 명령. 출력할값 없음
-        Setting(q)
-        #print(belts[1:])
-        #print(max_belts[1:])
-        #pass
+        print(q)
+        
     elif q[0] == 200: # 200 m_src m_dst
         Rule2(q)
-        #print(belts[1:])
-        #print(max_belts[1:])
+        
     elif q[0] == 300: # 300 m_src m_dst
         Rule3(q)
-        #print("================== Time : ", i, "==================")
-        #print(belts[1:])
-        #print(max_belts[1:])
+
     elif q[0] == 400: # 400 m_src m_dst
         Rule4(q)
-        #print("================== Time : ", i, "==================")
-        #print(belts[1:])
-        #print(max_belts[1:])
+        
     elif q[0] == 500: # 500 p_num
         Rule5(q)
-        #print("================== Time : ", i, "==================")
-        #print(belts[1:])
-        #print(max_belts[1:])
+        
     elif q[0] == 600: # 600 b_num
         Rule6(q)
-        #print("================== Time : ", i, "==================")
-        #print(belts[1:])
-        #print(max_belts[1:])
+        
     
     if i == stop:
-        print(q)
         print("NUM : ", nums[1:])
         print("HEAD : ", head[1:])
         print("TAIL : ", tail[1:])
