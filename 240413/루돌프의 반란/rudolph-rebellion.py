b9eeea9073afdbf15b9b78550a9c8a588c0c882d
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-04-13 17:43:32 +0900

    Title: 루돌프의 반란, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240413/루돌프의 반란/rudolph-rebellion.py
+++ 240413/루돌프의 반란/rudolph-rebellion.py
@@ -67,7 +67,7 @@ def RGo():
     for k, v in santas.items(): #idx, santa
         if v.out == 1: # out 인 산타는 제외
             continue
-        if dis > Distance(R, v):
+        if dis > Distance(R, v): # 더 가까운 santa로 갱신
             dis = Distance(R, v); close_santa = v
         elif dis == Distance(R,v):
             if close_santa < v: # r이 더 큰, c가 더큰
@@ -83,14 +83,15 @@ def RGo():
     
     R.r += dr[min_dis_dir]; R.c += dc[min_dis_dir] # 루돌프 이동시킴
     board[R.r][R.c] = -1 # 루돌프 board 위에서 이동시킴
-    R.d = min_dis_dir
+    R.d = min_dis_dir # 루돌프 방향 갱신
     if min_dis == 0: # 루돌프-산타 충돌
         Collapse(R, close_santa, 'r')
 
 def SantaGo():
     # 산타 움직임
-    global dr, dc, board, santas
-    for k, v in santas.items():
+    global dr, dc, board, santas, P
+    for i in range(1, P+1):
+        v = santas[i]
         if v.out == 1: #밖이면 고려X
             continue
         if v.stun > 0: # 이번 한 턴 쉬기
@@ -107,7 +108,7 @@ def SantaGo():
                         min_dis = (nr-R.r)**2 + (nc-R.c)**2
                         min_dis_dir = i
             
-        if min_dis_dir == -1: # 갈 곳이 없음
+        if min_dis_dir == -1: # 갈 곳이 없음. 다음 산타
             continue
         
         board[v.r][v.c] = 0 # 산타 이동 전 자리 제거
@@ -120,15 +121,16 @@ def SantaGo():
     return
 
 def Collapse(R, santa, opt):
-    global dr, dc, board
+    global dr, dc, board, OUT_
     # 충돌
     if opt == 'r': #루돌프가 와서 충돌
         #board[santa.r][santa.c] = 0 # 부딪힌 곳이라 루돌프가 차지함
         santa.getScore(R.power) # 산타 C점 얻음
         santa.r += R.power*dr[R.d]; santa.c += R.power*dc[R.d] # 루돌프 온 방향으로 밀려남
-        santa.d = R.d; santa.stun = 2
+        santa.d = R.d; santa.stun = 2 # 산타 방향 갱신, 부딪혀서 해당 산타 기절
         if santa.r <= 0 or santa.c <= 0 or santa.r >= N+1 or santa.c >= N+1: #밖임
             santa.out = 1 # 밖이라 out
+            OUT_ += 1
         elif board[santa.r][santa.c] != 0: # 다른 산타가 있음
             #print("Interact r santa : ", santa.idx)
             Interact(santa, santas[board[santa.r][santa.c]])
@@ -138,10 +140,11 @@ def Collapse(R, santa, opt):
     else: # opt = 's' # 산타가 와서 충돌
         #print("collapse santa info : ", santa.idx, santa.r, santa.c)
         santa.getScore(santa.power) # 산타 D점 얻음
-        santa.r -= santa.power*dr[santa.d]; santa.c -= santa.power*dc[santa.d]
-        santa.d = (santa.d+2)%4; santa.stun = 2
+        santa.r -= santa.power*dr[santa.d]; santa.c -= santa.power*dc[santa.d] # 루돌프로 가려던 방향 반대로 밀려남
+        santa.d = (santa.d+2)%4; santa.stun = 1 # 산타 방향 갱신, 부딪혀서 해당 산타 기절
         if santa.r <= 0 or santa.c <= 0 or santa.r >= N+1 or santa.c >= N+1: #밖임
             santa.out = 1 # 밖이라 out
+            OUT_ += 1
         elif board[santa.r][santa.c] != 0: # 다른 산타가 있음
             #print("Interact santa : ", santa.idx)
             Interact(santa, santas[board[santa.r][santa.c]])
@@ -158,8 +161,9 @@ def Interact(s1, s2):
     board[s1.r][s1.c] = s1.idx # 1이 2 자리를 먹음
     if s2.r <= 0 or s2.c <= 0 or s2.r >= N+1 or s2.c >= N+1: #밖임
         s2.out = 1
+        OUT_ += 1
     elif board[s2.r][s2.c] != 0: # 다른 산타가 있음
-        board[s2.r][s2.c] = s2.idx # 다른 산타가 앉음
+        #board[s2.r][s2.c] = s2.idx # 다른 산타가 앉음
         Interact(s2, board[s2.r][s2.c])
     elif board[s2.r][s2.c] == 0: # 산타가 없음
         board[s2.r][s2.c] = s2.idx # 산타 이동 완료
@@ -178,22 +182,37 @@ stop = -1
 show_board = 1
 show_rudolf = 1
 show_santas = 1
+
+if stop == 1:
+    print("before : ")
+    for i in range(1, N+1):
+        print(board[i][1:])
+
 for m in range(1, M+1):
+    if OUT_ >= P: # 산타가 모두 탈락
+        break
+    if m == stop:
+        print("========= round ", m)
+        print("before : ")
+        if show_board == 1:
+            for i in range(1, N+1):
+                print(board[i][1:])
 
     RGo()
     SantaGo()
     GetBonus()
     if m == stop:
+        print("after : ")
         if show_board == 1:
             for i in range(1, N+1):
                 print(board[i][1:])
         if show_rudolf == 1:
             print("rudolf info (r,c, d) :", R.getInfo())
         if show_santas == 1:
-            for k, v in santas.items():
-                print(k, "santa info (idx, r,c, stun, out, score, d) : ", v.getInfo())
+            for i in range(1, P+1):
+                print(i, "santa info (idx, r,c, stun, out, score, d) : ", santas[i].getInfo())
         break
 
 #print("rudolf info (r,c, power) :", R.getInfo())
-for k, v in santas.items():
-    print(v.score, end=" ")
+for i in range(1, P+1):
+    print(santas[i].score, end=" ")
