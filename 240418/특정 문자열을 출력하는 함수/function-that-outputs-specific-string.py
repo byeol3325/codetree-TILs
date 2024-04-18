3bdddb310b2e427c9f4a2248205bff747ef0b430
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-04-18 09:27:14 +0900

    Title: 특정 문자열을 출력하는 함수, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240418/특정 문자열을 출력하는 함수/function-that-outputs-specific-string.py
@@ -0,0 +1,13 @@
+def printAll():
+    print("Hello")
+    print("#@#@#@#@#@")
+    print("CodeTree")
+    print("@#@#@#@#@#")
+    print("Students!")
+    return
+
+N = int(input())
+
+for _ in range(N):
+    printAll()
+    print()
