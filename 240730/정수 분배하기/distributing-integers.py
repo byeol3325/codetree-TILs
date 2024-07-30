fe284c3c6ff0676bd9b010b84ba6d052aa435d3d
byeol3325 <34564290+byeol3325@users.noreply.github.com>
2024-07-30 17:41:23 +0900

    Title: 정수 분배하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240730/정수 분배하기/distributing-integers.py
@@ -0,0 +1,40 @@
+n, m = map(int, input().split())
+arr = [0]*n
+for i in range(n):
+    arr[i] = int(input())
+
+def parametric_search(arr: list, m: int):
+    left = 1
+    right = sum(arr)//m
+    n = len(arr)
+
+    Ks = []
+    while left < right:
+        mid = (left + right) // 2
+        cnt = 0
+
+        for i in range(n):
+            cnt += arr[i] // mid
+        
+        if cnt < m:
+            # 더 크게할 수 있음 = 나누는 mid를 줄여야함
+            right = mid-1
+        elif cnt >= m:
+            # 더 잘게 해야함 = 나누는 mid를 키워야함
+            left = mid+1
+            Ks.append(mid)
+        
+    
+    mid = (left + right) // 2
+    cnt = 0
+    for i in range(n):
+        cnt += arr[i] // mid
+
+    if cnt == m:
+        Ks.append(mid)
+    
+    return max(Ks)
+
+
+k = parametric_search(arr, m)
+print(k)
